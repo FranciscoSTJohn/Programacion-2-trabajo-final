4b9775a02ad52071097ee382f094f61d8a654b72
IriarteMateo <miriarte@udesa.edu.ar>
2023-04-05 16:41:09 -0300

    parte del data

---
+++ proyecto/data/index.js
@@ -0,0 +1,65 @@
+let  = {
+        usuario : {
+            nombreDeUsuario: 'Mateo',
+            apellido: 'Iriarte',
+            email: 'iriartemateo@gmail.com',
+            telefono: '1166641796',
+            fechaDeNacimiento: '01/05/2004'
+      },
+        productos: [ 
+
+        {
+          nombre: '' ,
+          imagen: '',
+          descripcion: '',
+          id: 1,
+          
+        },
+        
+        {
+            nombre: '' ,
+            imagen: '',
+            descripcion: '',
+            id: 2,
+      },{
+            nombre: '' ,
+            imagen: '',
+            descripcion: '',
+            id: 3,
+      },{
+            nombre: '' ,
+            imagen: '',
+            descripcion: '',
+            id: 4,
+      },{
+            nombre: '' ,
+            imagen: '',
+            descripcion: '',
+            id: 5,
+      },{
+            nombre: '' ,
+            imagen: '',
+            descripcion: '',
+            id: 6,
+      },{
+            nombre: '' ,
+            imagen: '',
+            descripcion: '',
+            id: 7,
+      },{
+            nombre: '' ,
+            imagen: '',
+            descripcion: '',
+            id: 8,
+      },{
+            nombre: '' ,
+            imagen: '',
+            descripcion: '',
+            id: 9,
+      },{
+            nombre: '' ,
+            imagen: '',
+            descripcion: '',
+            id: 10,
+        }
+          ]}
