1b8a190eda81863fa4b941fa0f85cefa172ca40e
Francisco St John <fstjohn@udesa.edu.ar>
2023-06-09 16:42:46 -0300

    buscador

--- proyecto/controllers/searchResultsController.js
+++ proyecto/controllers/searchResultsController.js
@@ -1,8 +1,22 @@
-let data = require ('../data/index')
+let db= require("../database/models");
+let op= db.sequelize.Op;
+
+
 let searchResultsController = {
     index: function(req, res){
-        let productos = data.productos
-        return res.render('search-results', {productos: productos, comentarios: data.comentarios})
+        let buscado=req.query.search;
+        
+        let filtrados={
+            where:{
+                [op]:[
+                    {nombre_producto:{[op.like]: `%${buscado}%`}},
+                    {descripcion:{[op.like]:`%${buscado}%`}}
+                ]
+            }
+        }
+        db.Producto.findall(filtrados)
+        .then((Producto))
+            return res.render('search-results', {Producto: nombre_producto})
     }
 }
 module.exports = searchResultsController
