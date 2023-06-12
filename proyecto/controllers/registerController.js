38d55c0149cd356f0317a7a5a25d3952fb06852b
IriarteMateo <miriarte@udesa.edu.ar>
2023-06-12 12:05:13 -0300

    register-arreglar condicionales

--- proyecto/controllers/registerController.js
+++ proyecto/controllers/registerController.js
@@ -5,34 +5,39 @@ let registerController = {
         return res.render('register', {})
     },
     store: function (req, res) {
+        let form = req.body
+        let errors = {};
+        if (form.email == '') {
+             errors.mensaje = "Tienes que ingresar un mail!"
+             res.locals.errors = errors; 
+             return res.render('register'); 
 
-         let form = req.body;
-         res.redirect('/')
-             
-        let user = {
-            email: form.mail, 
-            password: bcrypt.hashSync(form.contra,10),
-            nombreUsuarios: form.user,
-            fecha: form.fecha,
-            DNI: form.DNI
-            }
-
-        let errors = {}
-        if (form.mail == "") {
-            errors.mensaje = "Ingresa tu email!";
-            res.locals.error = errors;
-            return res.render('register'); 
         }
-
-        db.User.create(user)
-            .then(function(usuarioCreado){ 
-                console.log(usuarioCreado);
-                return res.redirect('/product');
-         })
-             .catch(function(error){
-                console.log(error);
+        else if (form.password == '') {
+            errors.mensaje = "Tienes que ingresar una contraseña!"
+            res.locals.errors = errors; 
+            return res.render ('register')
+        }
+        else {         
+           let user = {
+               email: form.mail, 
+               password: bcrypt.hashSync(form.contra,10),
+               nombreUsuarios: form.user,
+               fecha: form.fecha,   
+               DNI: form.DNI
+               }
+   
+           db.User.create(user)
+               .then(function(usuarioCreado){ 
+                   console.log(usuarioCreado);
+                   return res.redirect('/');
             })
-         }
+                .catch(function(error){
+                   console.log(error);
+               })
+       }
+    
+    }
     }    
 
 module.exports = registerController
